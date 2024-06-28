cfa8c6fca8db4f72d6d08b5eaab45b75c134bbca
tanavposwal <tanavposwal939@gmail.com>
2024-06-28 13:26:52 +0530

    trpc server, react init

--- server/index.ts
+++ server/index.ts
@@ -1,62 +1,49 @@
-import { publicProcedure, router } from "./trpc";
-import { z } from "zod";
+import { router } from './trpc';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import mongoose, { mongo } from 'mongoose';
+import { User , Todo} from "./db/schema";
+import jwt from "jsonwebtoken";
+import { userRouter } from './routers/user';
+import { todoRouter } from './routers/todo';
+import cors from "cors";
+export const SECRET = 'SECr3t';
+
+mongoose.connect('mongodb://localhost:27017/', { dbName: "todo" });
 
 const appRouter = router({
-    signUp: publicProcedure
-        .input(
-            z.object({
-                email: z.string(),
-                password: z.string(),
-            })
-        )
-        .mutation(async (opts) => {
-            let email = opts.input.email
-            let password = opts.input.password
+    user: userRouter,
+    todo: todoRouter
+})
 
-            // do db stuff here
-            // sign jwt token
-
-            return {
-                email, password
-            };
-        }),
-
-    createTodo: publicProcedure
-        .input(
-            z.object({
-                title: z.string(),
-                description: z.string()
-            })
-        )    
-        .mutation(async (opts) => {
-            const username = opts.ctx.username
-            console.log(username)
-            let title = opts.input.title
-            let description = opts.input.description
-            let done = false
-            // do db stuff here
-
-            return {
-                title, description, done
-            };
-        })
-});
-
+// Export type router type signature,
+// NOT the router itself.
+// make trpc type saf ein client
 export type AppRouter = typeof appRouter;
 
 const server = createHTTPServer({
-  router: appRouter,
-  createContext(opts) {
-    let authHeader = opts.req.headers["authorization"];
-    console.log(authHeader)
-    // jwt.verify
-    return {
-        username: "dummy"
-    }
-  }
-});
+    router: appRouter,
+    middleware: cors(),
+    createContext(opts) {
+        let authHeader = opts.req.headers["authorization"];
 
-// Even in express it is preferred to use context to pass username
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+            console.log(token);
+            return new Promise<{db: {Todo: typeof Todo, User: typeof User}, userId?: string}>((resolve) => {
+                jwt.verify(token, SECRET, (err, user: any) => {
+                    if (user) {
+                        resolve({userId: user.userId as string, db: {Todo, User}});
+                    } else {
+                        resolve({db: {Todo, User}});
+                    }
+                });
+            })
+        }
+
+        return {
+            db: {Todo, User},
+        }
+    }
+});
 
 server.listen(3000);
